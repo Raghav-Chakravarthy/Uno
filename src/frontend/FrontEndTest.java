c804d9ca0f4639c75fd01d15c975b22a2a13f372
Jiming-C <jimingchen2015@gmail.com>
2023-03-28 19:25:44 -0400

    intro screen, tester, icons, buttons

--- src/frontend/FrontEndTest.java
+++ src/frontend/FrontEndTest.java
@@ -1,37 +1,27 @@
 package frontend;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class FrontEndTest {
 
     private JFrame frm = new JFrame();
     private Intro pnl = new Intro();
-    private JButton btn = new JButton("Get ScreenSize for JComponents");
 
     public FrontEndTest() {
-        pnl.setPreferredSize(new Dimension(960, 600));
-        pnl.add(btn, BorderLayout.SOUTH);
+        frm.setSize(960, 600);
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frm.add(pnl, BorderLayout.CENTER);
-        frm.setLocation(150, 100);
-        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // EDIT
-        frm.setResizable(false);
-        frm.pack();
         frm.setVisible(true);
     }
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
-
             @Override
             public void run() {
-                FrontEndTest fS = new FrontEndTest();
+                FrontEndTest ft = new FrontEndTest();
             }
         });
     }
 }
+
--- src/frontend/Intro.java
+++ src/frontend/Intro.java
@@ -1,36 +1,97 @@
 package frontend;
 
-import javax.swing.*;
+
 import java.awt.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class Intro extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
-	public Intro() {
-		setBackground(Color.ORANGE);
-		setLayout(null);
-		
-//		Image img = new ImageIcon(this.getClass().getResource("/background.png")).getImage();
-//		
-//		JLabel wildCard = new JLabel("");
-//		img = new ImageIcon(this.getClass().getResource("/wildcard.png")).getImage();
-//		wildCard.setIcon(new ImageIcon(img));
-//		wildCard.setBounds(87, 237, 82, 129);
-//		add(wildCard);
-		
+    private Image image;
 
-		
-		//show background
-		this.repaint();
-	}
-	
-	public void paintComponent(Graphics g) {
-		
-		ImageIcon ic = new ImageIcon("/background.png");
-		Image i = ic.getImage();
-		
-		g.drawImage(i, 0, 0, this.getSize().width, this.getSize().height, this);
-	}
+    public Intro() {
+        // Load the image from the file path
+//         ImageIcon imageIcon = new ImageIcon(getClass().getResource("/red_button.png"));
+//         image = imageIcon.getImage();
+    	Image img = null;
+		try {
+			img = ImageIO.read(getClass().getResource("/green_button.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	ImageIcon icon = new ImageIcon(img);
+
+        setBackground(Color.RED);
+        setLayout(null);
+
+//        ImageIcon icon = new ImageIcon(getClass().getResource("/red_button.png"));
+        JButton button1 = new JButton(icon);
+        button1.setBounds(10, 482, 195, 75);
+        button1.addActionListener(new ButtonListener("Button 1"));
+        add(button1);
+
+//        icon = new ImageIcon(getClass().getResource("/red_button.png"));
+        JButton button2 = new JButton(icon);
+        button2.setBounds(257, 482, 195, 75);
+        button2.addActionListener(new ButtonListener("Button 2"));
+        add(button2);
+
+//        icon = new ImageIcon(getClass().getResource("/red_button.png"));
+        JButton button3 = new JButton(icon);
+        button3.setBounds(490, 482, 195, 75);
+        button3.addActionListener(new ButtonListener("Button 3"));
+        add(button3);
+
+//        icon = new ImageIcon(getClass().getResource("/path/to/image4.png"));
+        JButton button4 = new JButton(icon);
+        button4.setBounds(740, 482, 195, 75);
+        button4.addActionListener(new ButtonListener("Button 4"));
+        add(button4);
+        
+        JLabel logo = new JLabel("");
+		try {
+			img = ImageIO.read(getClass().getResource("/logo.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		icon = new ImageIcon(img);
+		logo.setIcon(icon);
+        logo.setBounds(0, 11, 960, 460);
+        add(logo);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Draw the image on the panel
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    private class ButtonListener implements ActionListener {
+
+        private String buttonName;
+
+        public ButtonListener(String buttonName) {
+            this.buttonName = buttonName;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Button " + buttonName + " clicked");
+        }
+    }
 }
