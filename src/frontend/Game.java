524ccb3f13b20c501bee30fc019cea4758b90c05
Jiming <71953568+Jiming-C@users.noreply.github.com>
2023-04-19 13:10:54 -0400

    drawing cards

--- src/frontend/Game.java
+++ src/frontend/Game.java
@@ -1,130 +1,61 @@
 package frontend;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+import java.awt.Point;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+public class Game extends JPanel implements MouseMotionListener {
+    private Image myImage;
+    private final String path = "assets" + File.separator + "cards" + File.separator;
+    public int x;
+    public int y;
+    //names of card files
+    private string[] cards = {"1_blue.png", "1_green.png", "1_red.png"};
+    //list of coordinates to draw the cards, corrsponds to the cards array
+    private location[] coordinates = {new Point(4, 13), new Point(24, 13), new Point(44, 13)};
 
-public class Game extends JPanel {
-
-    private Image image;
-
+    
     public Game() {
-        // Load the image from the file path
-//         ImageIcon imageIcon = new ImageIcon(getClass().getResource("/red_button.png"));
-//         image = imageIcon.getImage();
-    	Image img = null;
-		try {
-			img = ImageIO.read(getClass().getResource("/green_button.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	ImageIcon icon = new ImageIcon(img);
-
-        setBackground(Color.RED);
-        setLayout(null);
-        
-        JLabel player2Cards = new JLabel("Cards: 02");
-        player2Cards.setForeground(Color.ORANGE);
-        player2Cards.setFont(new Font("Calibri", Font.BOLD, 25));
-        player2Cards.setBounds(806, 87, 106, 31);
-        add(player2Cards);
-        
-        JLabel player3Cards = new JLabel("Cards: 12");
-        player3Cards.setForeground(Color.ORANGE);
-        player3Cards.setFont(new Font("Calibri", Font.BOLD, 25));
-        player3Cards.setBounds(23, 87, 106, 31);
-        add(player3Cards);
-        
-        JLabel player4Cards = new JLabel("Cards: 09");
-        player4Cards.setForeground(Color.ORANGE);
-        player4Cards.setFont(new Font("Calibri", Font.BOLD, 25));
-        player4Cards.setBounds(394, 87, 106, 31);
-        add(player4Cards);
-        
-        JLabel deck = new JLabel("");
-		try {
-			img = ImageIO.read(getClass().getResource("/deck.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		icon = new ImageIcon(img);
-		deck.setIcon(icon);
-		deck.setBounds(23, 249, 129, 124);
-        add(deck);
-        
-        JLabel card0 = new JLabel("");
-        card0.setBounds(23, 465, 80, 124);
-        add(card0);
-        
-        JLabel card1 = new JLabel("");
-        card1.setBounds(108, 465, 80, 124);
-        add(card1);
-        
-        JLabel card2 = new JLabel("");
-        card2.setBounds(198, 465, 80, 124);
-        add(card2);
-        
-        JLabel card3 = new JLabel("");
-        card3.setBounds(288, 465, 80, 124);
-        add(card3);
-        
-        JLabel card4 = new JLabel("");
-        card4.setBounds(378, 465, 80, 124);
-        add(card4);
-        
-        JLabel card5 = new JLabel("");
-        card5.setBounds(468, 465, 80, 124);
-        add(card5);
-        
-        JLabel card6 = new JLabel("");
-        card6.setBounds(558, 465, 80, 124);
-        add(card6);
-        
-        JLabel card7 = new JLabel("");
-        card7.setBounds(648, 465, 80, 124);
-        add(card7);
-        
-        JLabel card8 = new JLabel("");
-        card8.setBounds(738, 465, 80, 124);
-        add(card8);
-        
-        JLabel card9 = new JLabel("");
-        card9.setBounds(834, 465, 80, 124);
-        add(card9);
-        
-        JLabel PCard0_1 = new JLabel("");
-        PCard0_1.setBounds(468, 291, 80, 124);
-        add(PCard0_1);
-        
-        JLabel pile = new JLabel("");
-		try {
-			img = ImageIO.read(getClass().getResource("/pile.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		icon = new ImageIcon(img);
-		pile.setIcon(icon);
-        pile.setBounds(431, 272, 207, 165);
-        add(pile);
+        addMouseMotionListener(this);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Draw the image on the panel
-        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        Graphics2D g2d = (Graphics2D) g;
+        looping through all the cards
+        for (int i = 0; i < cards.length(); i++) {
+            Image img = new ImageIcon(path+ cards[i]).getImage();
+            g2d.drawImage(img, coordinates[i].getX, coordinates[i].getY, null);
+        }
+        
     }
-}
 
+    public void mouseMoved(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+
+        //for finding bounds
+        System.out.println("Mouse At: (" + x + ", " + y + ")");
+
+        //reloads the image and repaints
+        myImage = loadImage();
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+    // public static void main(String[] args) {
+    //     JFrame frame = new JFrame("Intro");
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     frame.setSize(975, 635);
+        
+    //     Intro intro = new Intro();
+    //     frame.add(intro);
+        
+    //     frame.setVisible(true);
+    // }
+}
